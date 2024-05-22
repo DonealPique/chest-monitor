008ea98920d7a386e640b23d79af2eec252c7a0b
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-05-22 14:40:18 +0200

    install

---
+++ install.lua
@@ -0,0 +1,28 @@
+local url = "https://github.com/DonealPique/chest-monitor/blob/main/chest_monitor.lua"
+
+print("Downloading chest monitor...")
+local response = http.get(url)
+if response then
+    local script = response.readAll()
+    response.close()
+end
+
+local file = fs.open("chest_monitor.lua", "w")
+file.write(script)
+file.close()
+print("Downloaded and saved as chest_monitor.lua")
+
+local startupScript = [[
+shell.run("chest_monitor.lua")
+]]
+
+local startupFile = fs.open("startup.lua", "w")
+startupFile.write(startupScript)
+startupFile.close()
+print("Created startup.lua to run chest_monitor.lua on startup")
+
+print("Installation complete. Rebooting......")
+os.reboot()
+else
+    print("Failed to download chest monitor. Check the URL and attempt again.")
+end
