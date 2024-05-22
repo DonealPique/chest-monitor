06a0a0b016a09251a9ad150f83ed13652cdea468
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-05-22 15:42:43 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -1,28 +1,26 @@
 local url = "https://raw.githubusercontent.com/DonealPique/chest-monitor/main/chest_monitor.lua"
 
-print("Downloading chest monitor...")
+print("Downloading chest_monitor.lua...")
 local response = http.get(url)
 if response then
     local script = response.readAll()
     response.close()
-end
 
-local file = fs.open("chest_monitor.lua", "w")
-file.write(script)
-file.close()
-print("Downloaded and saved as chest_monitor.lua")
+    local file = fs.open("chest_monitor.lua", "w")
+    file.write(script)
+    file.close()
+    print("Downloaded and saved as chest_monitor.lua")
 
-local startupScript = [[
-shell.run("chest_monitor.lua")
-]]
+    local startupScript = [[
+    shell.run("chest_monitor.lua")
+    ]]
+    local startupFile = fs.open("startup.lua", "w")
+    startupFile.write(startupScript)
+    startupFile.close()
+    print("Created startup.lua to run chest_monitor.lua at startup")
 
-local startupFile = fs.open("startup.lua", "w")
-startupFile.write(startupScript)
-startupFile.close()
-print("Created startup.lua to run chest_monitor.lua on startup")
-
-print("Installation complete. Rebooting......")
-os.reboot()
+    print("Installation complete. Rebooting...")
+    os.reboot()
 else
-    print("Failed to download chest monitor. Check the URL and attempt again.")
+    print("Failed to download chest_monitor.lua. Check the URL and try again.")
 end
