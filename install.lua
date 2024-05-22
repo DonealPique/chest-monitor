ccf305afc87b5ff3c27bc504fd9205c6a20fab62
Doneal Piqué <135713732+DonealPique@users.noreply.github.com>
2024-05-22 15:47:00 +0200

    Update install.lua

--- install.lua
+++ install.lua
@@ -6,21 +6,29 @@ if response then
     local script = response.readAll()
     response.close()
 
-    local file = fs.open("chest_monitor.lua", "w")
-    file.write(script)
-    file.close()
-    print("Downloaded and saved as chest_monitor.lua")
+    local file = fs.open("/chest_monitor.lua", "w")
+    if file then
+        file.write(script)
+        file.close()
+        print("Downloaded and saved as chest_monitor.lua")
 
-    local startupScript = [[
-    shell.run("chest_monitor.lua")
-    ]]
-    local startupFile = fs.open("startup.lua", "w")
-    startupFile.write(startupScript)
-    startupFile.close()
-    print("Created startup.lua to run chest_monitor.lua at startup")
+        local startupScript = [[
+        shell.run("/chest_monitor.lua")
+        ]]
+        local startupFile = fs.open("/startup.lua", "w")
+        if startupFile then
+            startupFile.write(startupScript)
+            startupFile.close()
+            print("Created startup.lua to run chest_monitor.lua at startup")
 
-    print("Installation complete. Rebooting...")
-    os.reboot()
+            print("Installation complete. Rebooting...")
+            os.reboot()
+        else
+            print("Error: Could not create startup.lua")
+        end
+    else
+        print("Error: Could not save chest_monitor.lua")
+    end
 else
     print("Failed to download chest_monitor.lua. Check the URL and try again.")
 end
